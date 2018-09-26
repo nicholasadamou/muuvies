c94231c3e1a24307d74d03b43cf83c1555d97605
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-09-26 09:59:51 -0500

    added Homepage with React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import Home from './routes/Home.js';
 
 class App extends Component {
   render() {
-    return ({/**/});
+    return (
+      <Router>
+        <Route exact path="/" component={Home}/>
+      </Router>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
